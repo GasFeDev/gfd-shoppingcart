82fb6c6e3cd60c0299b0e125ead9fbf4923485b5
GasFeDev <gasfedev@gmail.com>
2022-09-01 09:54:12 -0300

    deploy 1

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,4 +1,5 @@
-import { axiosInstance } from "./config";
+/* import { axiosInstance } from "./config"; */
+import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
 
@@ -11,11 +12,11 @@ const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
 
-export const publicRequest = axiosInstance.create({
+export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
 
-export const userRequest = axiosInstance.create({
+export const userRequest = axios.create({
   baseURL: BASE_URL,
   header: { token: `Bearer ${TOKEN}` },
 });
