e42240f52d16b98e5cd16b3163a74dc92f34ab6c
GasFeDev <gasfedev@gmail.com>
2022-09-01 11:51:31 -0300

    deploy 1

--- client/src/redux/apiCalls.js
+++ client/src/redux/apiCalls.js
@@ -1,10 +1,15 @@
 import { loginFailure, loginStart, loginSuccess } from "./userRedux";
+import axios from "axios";
 import { publicRequest } from "../requestMethods";
+
+const axiosInstance = axios.create({
+  BASE_URL: process.env.REACT_APP_API_URL,
+});
 
 export const login = async (dispatch, user) => {
   dispatch(loginStart());
   try {
-    const res = await publicRequest.post("/auth/login", user);
+    const res = await axiosInstance.post("/auth/login", user);
     dispatch(loginSuccess(res.data));
   } catch (err) {
     dispatch(loginFailure());
